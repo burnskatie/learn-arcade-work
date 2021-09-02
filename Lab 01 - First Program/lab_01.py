b3a6312335eb32a2947887875958b19a619f4818
Katie Burns <katie.burns@my.simpson.edu>
2021-09-02 15:29:18 -0500

    Work on Lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,8 @@
+print("My name is Katie\nBurns and I am from Des Moines")
+print("I have a twin sister, and a\nfive year old brother.")
+print("I am Graphic Design Major with a Marketing Communications minor.")
+print("""Fun fact!
+I am
+the marketing intern
+with Conterra
+Ag Captial.""")
