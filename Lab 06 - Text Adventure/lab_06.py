ea1f1b6943d226535ee04ba39aad2e41f4cdc64d
Katie Burns <katie.burns@my.simpson.edu>
2021-10-20 12:07:36 -0500

    Revision of Lab 6

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -10,59 +10,80 @@ class Room:
 
 def main():
     room_list = []
-    my_room = Room("""You have entered the dusty castle hallway. 
-There are rooms to the north and south."""
+    my_room = Room("""You have entered the dusty castle hallway. There are rooms to the north and south."""
                    , 2, 4, 5, None)
-    room_list.append(my_room)
 
-    my_room = Room("""You have entered the King's room. 
-There is a hallway to the north."""
+    room_list.append(my_room)
+    my_room = Room("""You have entered the King's room. There is a hallway to the north."""
                    , 5, None, None, 4)
-    room_list.append(my_room)
 
-    my_room = Room("""You have entered the Queen's room. 
-She is getting ready for the ball.
-There is a hallway to the south."""
+    room_list.append(my_room)
+    my_room = Room("""You have entered the Queen's room. She is getting ready for the ball.
+    There is a hallway to the south."""
                    , None, 0, 3, None)
-    room_list.append(my_room)
 
+    room_list.append(my_room)
     my_room = Room("""You have made it to the Maid's quarters. 
-Tons of toilet paper and cleaning supplies.
-There is a hallway to the south"""
+    Tons of toilet paper and cleaning supplies. There is a hallway to the south"""
                    , None, 5, None, 2)
+
     room_list.append(my_room)
-
-
     my_room = Room("""You have entered into a bathroom.
-There is a hallway to the north.""", 0, None, 1, None)
-    room_list.append(my_room)
+    There is a hallway to the north.""", 0, None, 1, None)
 
+    room_list.append(my_room)
     my_room = Room("""You are in a pitch black hallway.
-There is a walkout to the east.
-There are rooms to the north and south."""
-                   , 3, 1, 6, 0)
-    room_list.append(my_room)
+    There is a walkout to the east.
+    There are rooms to the north and south.""", 3, 1, 6, 0)
 
+    room_list.append(my_room)
     my_room = Room("""You have walked out to the balcony.
-There is a hallway to the west.""", None, None, None, 5)
+    There is a hallway to the west.""", None, None, None, 5)
     room_list.append(my_room)
 
     current_room = 0
-
     done = False
 
     while not done:
-        choose = input("Where would you like to go? ")
-        if choose == "q":
-            print("Goodbye, you left the game.")
+
+        print()
+        print(room_list[current_room].description)
+        choice = input("What will you do? ")
+
+        if choice.upper() == "QUIT":
+            print("Thank you for playing")
             done = True
 
+        elif choice.upper() == "N" or choice.upper() == "North":
+            next_room = room_list[current_room].north
+            if next_room is None and not done:
+                print("You cannot go that way!")
+            else:
+                current_room = next_room
 
-    print(room_list [current_room].description)
+        elif choice.upper() == "S" or choice.upper() == "South":
+            next_room = room_list[current_room].south
+            if next_room is None and not done:
+                print("You cannot go that way!")
+            else:
+                current_room = next_room
 
+        elif choice.upper() == "E" or choice.upper() == "East":
+            next_room = room_list[current_room].east
+            if next_room is None and not done:
+                print("You cannot go that way!")
+            else:
+                current_room = next_room
 
+        elif choice.upper() == "W" or choice.upper() == "West":
+            next_room = room_list[current_room].west
+            if next_room is None and not done:
+                print("You cannot go that way!")
+            else:
+                current_room = next_room
 
-
+        else:
+            print("I don't know what you are saying, try saying something else.")
 
 
 main()
